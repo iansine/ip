3ae944d0d6138d5fe13e0f35702447332376e2f4
Ian Sin <iansin291@gmail.com>
2026-08-20 22:01:08 +0800

    Renamed the chatbot to Sine

---
+++ src/main/java/Sine.java
@@ -0,0 +1,13 @@
+/**
+ * Starts the Sine chatbot application.
+ */
+public class Sine {
+    public static void main(String[] args) {
+        String banner = " ____  _            \n"
+                + "/ ___|(_)_ __   ___ \n"
+                + "\\___ \\| | '_ \\ / _ \\\n"
+                + " ___) | | | | |  __/\n"
+                + "|____/|_|_| |_|\\___|\n";
+        System.out.println(banner);
+    }
+}
